e7e04c73acc675e1134149e3c5c92602300729a0
Esteban Abarca <eabarca171@gmail.com>
2023-01-13 15:47:28 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    ToolbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
